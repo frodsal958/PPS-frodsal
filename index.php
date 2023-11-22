c549f3edf76e3887cdc0a9dd6d6eac937236a1c4
frodsal <frodsal958@g.educaand.es>
2023-11-22 20:24:29 +0100

    pasar los archivos nuevos

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head></head>
+
+<body>
+	<?php
+	echo "Hola Mundo frodsal";
+	?>
+</body>
+
+</html>
+Esto es para que el commit 2º se genere y se guarde dentro de nuestra prueba php
+Esto es la prueba de que tenemos que reactivar la Web, vamos a ver el COMMIT
